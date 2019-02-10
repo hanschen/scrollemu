872bab0848debd4a91305eb7055df79de99372e5
Hans Chen <contact@hanschen.org>
2019-02-10 17:27:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+SCROLLEMU
+=========
+
+*scrollemu* is a simple utility that allows you to scroll by holding down a
+keyboard modifier key and move your mouse cursor. This is especially nice if
+you use a trackball or a pointing stick, but it works for all pointing devices.
+
+I quickly put this script together to make it easier to scroll with my
+trackball. It works well on my system without any noticeable impacts on
+performance. Your mileage may vary.
+
+
+Requirements
+------------
+
+* Python 3 or 2
+* [pynput]
+
+The script should work on all major platforms (Linux with Xorg, Windows,
+macOS) but has only been tested on Linux. Wayland is currently not supported.
+
+[pynput]: https://pypi.org/project/pynput/
+
+
+Install
+-------
+
+There is no special install procedure. Simply clone the Git repository:
+
+    git clone https://github.com/hanschen/scrollemu.git
+
+and run the `scrollemu.py` script inside the cloned directory.
+Optionally, you can add the `scrollemu` directory to your ``PATH``.
+
+
+Usage
+-----
+
+    scrollemu.py
+
+The script will run until killed. If you find that it works well, you can add
+`scrollemu.py` to autostart.
+
+
+Options
+-------
+
+Take a look at the top of the `scrollemu.py` script. In particular, you may
+want to change the `SCROLL_KEY` and `SENSITIVITY` options.
+
+If scrolling acts weird, try to disable `ACCELERATION`.
+
+
+Known issues and future plans
+-----------------------------
+
+*Mouse cursor flickers when scrolling.*
+No idea how to solve this issue, any solutions would be greatly appreciated.
+
+The current script works well for my needs and I don't plan to put much effort
+into updating it further. With that said, [pull requests] are always welcome!
+
+[pull requests]: https://github.com/hanschen/scrollemu/pulls
+
+
+Contact
+-------
+
+Author: Hans Chen (contact@hanschen.org)
+
+Website: https://github.com/hanschen/scrollemu
---
+++ scrollemu.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+"""Small utility that allows you to scroll by moving the mouse while holding
+down a keyboard modifier key."""
+import math
+
+from pynput import mouse, keyboard
+from pynput.keyboard import Key
+
+
+#
+# OPTIONS
+#
+
+# Keyboard modifier key to hold down to start scrolling.
+#
+# Examples:
+#   Key.cmd - Super/Windows/Command key
+#   Key.ctrl - Ctrl key
+#   Key.alt - Alt key
+#   Key.shift - Shift key
+#
+# Add _l or _r to the end to specify only the left or right key.
+#
+# Default: Key.cmd_l
+SCROLL_KEY = Key.cmd_l
+
+# Number of lines to scroll per pixels moved.
+# Can be smaller than 1 but must be larger than 0.
+#
+# Default: 0.05
+SENSITIVITY = 0.05
+
+# Enable scrolling in vertical direction.
+#
+# Default: True
+SCROLL_Y = True
+
+# Enable scrolling in horizontal direction.
+#
+# Default: True
+SCROLL_X = True
+
+# Invert scrolling direction in vertical direction.
+#
+# Default: False
+INVERT_Y = False
+
+# Invert scrolling direction in horizontal direction.
+#
+# Default: False
+INVERT_X = False
+
+# Enable scroll acceleration (larger mouse movement -> more scrolling).
+#
+# Default: True
+ACCELERATION = True
+
+
+class ScrollEmu(object):
+    def __init__(self):
+        self.mouse = mouse.Controller()
+        self.scroll_on = False
+        self.mouse_displacement_x = 0
+        self.mouse_displacement_y = 0
+        self.mouse_lock_position = self.mouse.position
+
+    @staticmethod
+    def acceleration(displacement):
+        """Return acceleration term based on displacement."""
+        # A determines when the acceleration starts to kick into effect
+        # (larger A -> more noticeable acceleration at smaller displacements)
+        A = 0.0005
+
+        # B controls the "steepness" of the acceleration curve
+        # (larger B -> larger acceleration at larger displacements)
+        B = 1.6
+
+        return A * abs(displacement)**B
+
+    def on_move(self, x, y):
+        if not self.scroll_on:
+            return
+
+        lock_x, lock_y = self.mouse_lock_position
+
+        # Do not do anything when resetting mouse position
+        if x == lock_x and y == lock_y:
+            return
+
+        self.mouse.position = self.mouse_lock_position
+
+        dy = y - lock_y
+        dx = x - lock_x
+
+        self.mouse_displacement_x = self.mouse_displacement_x + dx
+        self.mouse_displacement_y = self.mouse_displacement_y + dy
+
+        if SCROLL_Y and abs(self.mouse_displacement_y) >= 1./SENSITIVITY:
+            self.scroll_y()
+
+        if SCROLL_X and abs(self.mouse_displacement_x) >= 1./SENSITIVITY:
+            self.scroll_x()
+
+    def on_press(self, key):
+        if key == SCROLL_KEY:
+            self.scroll_on = True
+            self.mouse_displacement_x = 0
+            self.mouse_displacement_y = 0
+            self.mouse_lock_position = self.mouse.position
+
+    def on_release(self, key):
+        if key == SCROLL_KEY:
+            self.scroll_on = False
+
+    def scroll_lines(self, displacement, invert=False):
+        """Return number of lines to scroll based on displacement."""
+        scroll_lines = max([SENSITIVITY, 1])
+
+        if ACCELERATION:
+            acceleration = abs(self.acceleration(displacement))
+            scroll_lines = scroll_lines + acceleration
+
+        scroll_lines = math.copysign(scroll_lines, displacement)
+        if invert:
+            scroll_lines = -scroll_lines
+
+        return scroll_lines
+
+    def scroll_x(self):
+        scroll_lines = self.scroll_lines(self.mouse_displacement_x, INVERT_X)
+        self.mouse_displacement_x = 0
+        self.mouse.scroll(scroll_lines, 0)
+
+    def scroll_y(self):
+        scroll_lines = self.scroll_lines(self.mouse_displacement_y, INVERT_Y)
+
+        # Positive scroll = scroll down, need to reverse scroll direction
+        scroll_lines = -scroll_lines
+
+        self.mouse_displacement_y = 0
+        self.mouse.scroll(0, scroll_lines)
+
+    def run(self):
+        key_listener = keyboard.Listener(on_press=self.on_press,
+                                         on_release=self.on_release)
+        key_listener.start()
+        with mouse.Listener(on_move=self.on_move) as listener:
+            listener.join()
+
+
+if __name__ == "__main__":
+    program = ScrollEmu()
+    program.run()
